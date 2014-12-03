3a1f93fdd5ef82a2fbb3e8fa57f6e43750982868
Jeremy Vandehey <jgvandehey@gmail.com>
2014-12-02 16:28:56 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wegrow::Application.config.secret_token = 'd83e43d86278280f564e2dc141542da3e90f3494a453bc0878e666ec34b68cebd73d187f1c4d7d757f332c81dc889858440a2dd8c3053c04d050fbbe32dd8faf'
