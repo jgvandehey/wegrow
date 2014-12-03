4ceda0c1f9790afd82d818eb33aac72a6511ac5c
Jeremy Vandehey <jgvandehey@gmail.com>
2014-12-03 10:39:41 -0800

    landing page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
+	before_filter :disable_nav, only: [:home, :about]  #hiding nav on certain pages re: http://stackoverflow.com/questions/13395153/how-to-render-partial-on-everything-except-a-certain-action
   protect_from_forgery
+
+  	def disable_nav
+		@disable_nav = true
+	end
 end
